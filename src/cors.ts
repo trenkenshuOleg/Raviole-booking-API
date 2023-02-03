341a3d8689cb7f506f2c327dc06a56554b7bd5d0
trenkenshu <trenkenshu@gmail.com>
2023-02-03 16:36:32 +0300

    fix: add cors headers #3

--- src/cors.ts
+++ src/cors.ts
@@ -3,7 +3,7 @@ import {Request, Response, NextFunction} from 'express';
 
 const headers = (req: Request, res: Response, next: NextFunction) => {
 	console.log(req.headers);
-    const origin = (req.headers.origin == 'http://localhost:3000') ? 'http://localhost:3000' : 'https://mywebsite.com'
+    const origin = req.headers.origin ? req.headers.origin : ''
 	res.setHeader('Access-Control-Allow-Origin', origin)
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
