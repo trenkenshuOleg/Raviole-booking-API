e804d1151a9c5bd25dda76d498dd97a0e4b14dbd
trenkenshu <trenkenshu@gmail.com>
2023-02-05 23:18:27 +0300

    fix: add catch to sefInvoke

--- src/cors.ts
+++ src/cors.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import {Request, Response, NextFunction} from 'express';
 
 const headers = (req: Request, res: Response, next: NextFunction) => {
-	console.log(req.headers);
     const origin = req.headers.origin ? req.headers.origin : ''
 	res.setHeader('Access-Control-Allow-Origin', origin)
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { PrismaClient, Prisma, Client, Cafe } from '@prisma/client';
 import express = require('express');
 import headers from './cors';
-import db from './Restaurants_db';
-const path = require('path')
+// import db from './Restaurants_db';
+import path from 'path';
 
 
 const prisma = new PrismaClient();
@@ -48,46 +48,46 @@ server.post(`/register`, async (req, res) => {
 
   })
 
-server.post(`/cafe/new`, async (req, res) => {
-    const { name, city, phone, coordinates, averageCheck, images, menuImg, rating, workTimeStart, workTimeEnd, translation } = req.body
+// server.post(`/cafe/new`, async (req, res) => {
+//     const { name, city, phone, coordinates, averageCheck, images, menuImg, rating, workTimeStart, workTimeEnd, translation } = req.body
 
-    const cafes: Cafe[] = await prisma.cafe.findMany({
-        where: {
-                AND: [{
-                        name: name,
-                    },
-                    {
-                        city: city,
-                }]
-                }
-    });
-    console.log('existing', cafes);
-    const imgs = images as string[];
-    const menuImgs = menuImg as string[];
-    const coord = coordinates as number[];
+//     const cafes: Cafe[] = await prisma.cafe.findMany({
+//         where: {
+//                 AND: [{
+//                         name: name,
+//                     },
+//                     {
+//                         city: city,
+//                 }]
+//                 }
+//     });
+//     console.log('existing', cafes);
+//     const imgs = images as string[];
+//     const menuImgs = menuImg as string[];
+//     const coord = coordinates as number[];
 
-    if(cafes.length === 0) {
-        const result = await prisma.cafe.create({
-        data: {
-            name: String(name),
-            city: String(city),
-            phone: String(phone),
-            coordinates: coord,
-            averageCheck: Number(averageCheck),
-            images: imgs,
-            menuImg: menuImgs,
-            rating: Number(rating) || undefined,
-            workTimeStart: Number(workTimeStart),
-            workTimeEnd: Number(workTimeEnd),
-            translation: String(translation),
-        },
-        })
-        res.json(result);
-    } else {
-        res.json('{"error":"login or email is already taken"}')
-    }
+//     if(cafes.length === 0) {
+//         const result = await prisma.cafe.create({
+//         data: {
+//             name: String(name),
+//             city: String(city),
+//             phone: String(phone),
+//             coordinates: coord,
+//             averageCheck: Number(averageCheck),
+//             images: imgs,
+//             menuImg: menuImgs,
+//             rating: Number(rating) || undefined,
+//             workTimeStart: Number(workTimeStart),
+//             workTimeEnd: Number(workTimeEnd),
+//             translation: String(translation),
+//         },
+//         })
+//         res.json(result);
+//     } else {
+//         res.json('{"error":"login or email is already taken"}')
+//     }
 
-  })
+//   })
 
 server.get(`/cafe/:id`, async (req, res) => {
     const { id } = req.params;
@@ -121,24 +121,6 @@ server.get('/cafe', async (req, res) => {
     const { city, cuisineType, averageCheck, rating, tags } = req.query;
     const arrTags: string[] = String(tags).split('↕');
     const cuisineArr: string[] = String(cuisineType).split('↕');
-
-    // const tagsFilter = tags
-    // ? {
-    //     tags: {
-    //         hasEvery: arrTags
-    //     }
-    //   }
-    // :
-    //   {};
-
-    // const cuisineFilter = cuisineType
-    // ? {
-    //     cuisineType: {
-    //         hasEvery: cuisineArr
-    //     }
-    //   }
-    // :
-    //   {};
 
     const checkFilter = averageCheck
     ? {
@@ -191,28 +173,29 @@ server.get('/clients', async (req, res) => {
     // res.json('{"error":"login or email is not unique"}')
 })
 
-server.get('/upload', async (req, res) => {
-    db.forEach(async el => {
-        el.translation = JSON.stringify(el.translation);
-        const result = await prisma.cafe.create({
-            data: {
-                name: el.name,
-                city: el.city,
-                phone: el.phone,
-                coordinates: el.coordinates,
-                averageCheck: el.averageCheck,
-                images: el.images,
-                menuImg: el.menuImg,
-                rating: el.rating,
-                workTimeStart: el.workTimeStart,
-                workTimeEnd: el.workTimeEnd,
-                translation: JSON.stringify(el.translation),
-            },
-            })
-            res.json(result);
-    });
+// server.get('/upload', async (req, res) => {
+//     let ans: Cafe[] = [];
+//     db.forEach(async el => {
+//         console.log('creating', el.name, el.city);
+//         const result = await prisma.cafe.create({
+//             data: {
+//                 name: el.name,
+//                 city: el.city,
+//                 phone: el.phone,
+//                 coordinates: el.coordinates,
+//                 averageCheck: el.averageCheck,
+//                 images: el.images,
+//                 menuImg: el.menuImg,
+//                 rating: el.rating,
+//                 workTimeStart: el.workTimeStart,
+//                 workTimeEnd: el.workTimeEnd,
+//                 translation: JSON.stringify(el.translation),
+//             },
+//             })
+//             console.log(result);
+//     });
 
-})
+// })
 
 const worker = server.listen(3003, () =>
   console.log(`
@@ -230,6 +213,7 @@ const selfInvoke = () => {
         method: 'GET'
     })
     .then(async (res: Response) => console.log(res.status, url, 'invoked:', Date()))
+    .catch((err: Error) => console.log(err.message))
 
     setTimeout(selfInvoke, (Math.floor(Math.random() * 30) + 120) * 1000);
 }
