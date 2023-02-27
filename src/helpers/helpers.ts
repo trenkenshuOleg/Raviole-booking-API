4cae340f9b22d6c1386f99dac08dd51621f2bb8f
trenkenshu <trenkenshu@gmail.com>
2023-02-27 15:54:50 +0300

    feat: refactor and and error handling

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
-import { PrismaClient, Cafe, Prisma, Review } from '@prisma/client';
+import { PrismaClient, Cafe, Prisma, Review, Client } from '@prisma/client';
+import db from '../db_helpers/restaurants_db';
 
 const prisma = new PrismaClient();
 
@@ -10,6 +11,78 @@ const headers = (req: Request, res: Response, next: NextFunction) => {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
     res.setHeader('Access-Control-Allow-Credentials', 'true')
     next()
+}
+
+const registerUser = async (req: Request, res: Response) => {
+    const { login, password, email, phone } = req.body;
+
+    const clients: Client[] = await prisma.client.findMany({
+        where: {
+            OR: [
+                {
+                    login: login,
+                },
+                {
+                    email: email,
+                },
+            ]
+        }
+    });
+    console.log('existing', clients);
+    console.log('req', req.body);
+
+    try {
+        if (clients.length === 0) {
+            const result: Client = await prisma.client.create({
+                data: {
+                    login,
+                    password,
+                    email,
+                    phone,
+                    bonusPoints: 0,
+                },
+                include: { favourites: true, reviews: true, bookings: true },
+            })
+            res.json(result)
+        } else {
+            res.json('{"error":"login or email is already taken"}')
+        }
+    }catch(e: unknown) {
+        res.json(`{"error":"provided wrong or no registration data"}`)
+    }
+}
+
+const loginUser = async (req: Request, res: Response) => {
+    const { login, password } = req.body;
+
+        const client: Client[] = await prisma.client.findMany({
+            where: {
+                AND: [
+                    {
+                        login: String(login),
+                    },
+                    {
+                        password: String(password),
+                    },
+                ]
+            },
+            include: {
+                favourites: true,
+                reviews: {
+                    include: {
+                        cafe: true,
+                    }
+                }, bookings: {
+                    include: {
+                        cafe: true,
+                    }
+                }},
+        });
+        if (client.length === 1) {
+            res.json(client[0]);
+        } else {
+            res.json('{"error":"wrong username or password"}')
+        }
 }
 
 const restWithUdatedRating = async (data: Cafe & { reviews: Review[]},) => {
@@ -113,7 +186,6 @@ const updateFavourites = async (req: Request, res: Response) => {
             } }
     });
 
-    console.log(addFavourite);
     res.json(addFavourite);
 }
 
@@ -197,6 +269,152 @@ const updateReview = async (req: Request, res: Response) => {
 
 }
 
+const createCafe = async (req: Request, res: Response) => {
+    const { name, city, phone, coordinates, averageCheck, images, menuImg, rating, workTimeStart, workTimeEnd, translation } = req.body
+
+    const cafes: Cafe[] = await prisma.cafe.findMany({
+        where: {
+                AND: [{
+                        name: name,
+                    },
+                    {
+                        city: city,
+                }]
+                }
+    });
+    console.log('existing', cafes);
+    const imgs = images as string[];
+    const menuImgs = menuImg as string[];
+    const coord = coordinates as number[];
+
+    if(cafes.length === 0) {
+        const result = await prisma.cafe.create({
+        data: {
+            name: String(name),
+            city: String(city),
+            phone: String(phone),
+            coordinates: coord,
+            averageCheck: Number(averageCheck),
+            images: imgs,
+            menuImg: menuImgs,
+            rating: Number(rating) || undefined,
+            workTimeStart: Number(workTimeStart),
+            workTimeEnd: Number(workTimeEnd),
+            translation: String(translation),
+        },
+        })
+        res.json(result);
+    } else {
+        res.json('{"error":"Already have cafe with this name in this city"}')
+    }
+
+  }
+
+  const getClientById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (isNaN(Number(id))) {
+        res.json(`{"error":"no such user id ${id} is NaN"}`)
+    } else {
+        const client: Client | null = await prisma.client.findUnique({
+            where: {
+                id: Number(id),
+            },
+            include: {
+                favourites: {
+                    include: {
+                        favourClients: true,
+                    }
+                },
+                reviews: {
+                    include: {
+                        cafe: true,
+                    },
+                    orderBy: {
+                        rating: 'asc'
+                    }
+                },
+                bookings: {
+                    include: {
+                        cafe: true,
+                    },
+                },
+            }
+        });
+        if (client) {
+            res.json(client);
+        } else {
+            res.json(`{"error":"no such user id ${id}"}`)
+        }
+    }
+
+}
+
+const editClient = async (req: Request, res: Response) => {
+    const { id, email, phone, password } = req.body;
+    console.log('client/edit', id, email, phone, password)
+    const passObj =
+        password
+            ? { password }
+            : {};
+    const phoneObj =
+        phone
+            ? { phone }
+            : {};
+    const emailObj =
+        email
+            ? { email }
+            : {};
+
+    const result: Client | null = await prisma.client.update({
+        where: {
+            id: Number(id)
+        },
+        data: {
+            ...passObj,
+            ...emailObj,
+            ...phoneObj,
+        },
+    })
+
+    res.json(result)
+}
+
+const getCafeById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (isNaN(Number(id))) {
+        res.json(`{"error":"no such cafe id ${id} is NaN"}`)
+    } else {
+        const cafe: Cafe | null = await prisma.cafe.findUnique({
+            where: {
+                id: Number(id),
+            },
+            include: {
+                favourClients: true,
+                reviews: {
+                    include: {
+                        author: {
+                            include: {
+                                reviews: true,
+                            }
+                        },
+                    }
+                },
+                bookings: {
+                    include: {
+                        guest: true,
+                    }
+                }
+            }
+        });
+        if (cafe) {
+            res.json(cafe);
+        } else {
+            res.json(`{"error":"no such cafe with id ${id}"}`)
+        }
+    }
+
+}
+
 const createBooking = async (req: Request, res: Response) => {
     const { clientId, cafeId, tableId, date, duration, guestPhone, guestName, guestAmount } = req.body;
 
@@ -263,9 +481,10 @@ const updateBooking = async (req: Request, res: Response) => {
 
 const getByCity = async (req: Request, res: Response) => {
     const { city } = req.params;
+
     const cafe: Cafe[] = await prisma.cafe.findMany({
         where: {
-            city: String(city),
+            city: String(city[0].toUpperCase() + city.slice(1).toLowerCase()),
         },
         include: {
             bookings: {
@@ -280,13 +499,102 @@ const getByCity = async (req: Request, res: Response) => {
             }
         }
     });
-    if (cafe) {
+    if (cafe.length > 0) {
         res.json(cafe);
     } else {
-        res.json(`{"error":"no cafe in ${city}"}`)
+        res.json(`{"error":"no cafe in this city available: ${city}"}`)
     }
 }
 
+const getCafeFiltered = async (req: Request, res: Response) => {
+    const { city, averageCheck, rating } = req.query;
+    console.log('get /cafe', city, averageCheck, rating);
 
-const loader = { headers, createReview, updateFavourites, updateReview, createBooking, updateBooking, getByCity}
+    const checkFilter = averageCheck
+        ? {
+            averageCheck: {
+                lte: Number(averageCheck)
+            }
+        }
+        :
+        {};
+
+    const ratingFilter = rating
+        ? {
+            rating: {
+                gte: Number(rating)
+            }
+        }
+        :
+        {};
+
+    const ans: Cafe[] = await prisma.cafe.findMany({
+        where: {
+            AND: [
+                {
+                    city: city as string || undefined
+                },
+                { ...checkFilter },
+                { ...ratingFilter },
+            ]
+        }
+    })
+    console.log('answer /cafe', ans)
+    res.json(ans);
+}
+
+const upadatePartial = async (req: Request, res: Response) => {
+    const upd: Cafe = await prisma.cafe.update({
+        where: {
+            id: 2
+        },
+        data: {
+        }
+    });
+
+    res.json(upd)
+}
+
+const uploadDb = async () => {
+    db.forEach(async el => {
+        console.log('creating', el.name, el.city);
+        const result = await prisma.cafe.create({
+            data: {
+                name: el.name,
+                city: el.city,
+                phone: el.phone,
+                coordinates: el.coordinates,
+                averageCheck: el.averageCheck,
+                images: el.images,
+                menuImg: el.menuImg,
+                rating: el.rating,
+                workTimeStart: el.workTimeStart,
+                workTimeEnd: el.workTimeEnd,
+                translation: JSON.stringify(el.translation),
+            },
+            })
+            console.log(result);
+    });
+
+}
+
+
+const loader = {
+    headers,
+    registerUser,
+    loginUser,
+    getClientById,
+    editClient,
+    getCafeById,
+    createCafe,
+    createReview,
+    updateFavourites,
+    updateReview,
+    createBooking,
+    updateBooking,
+    getByCity,
+    getCafeFiltered,
+    upadatePartial,
+    uploadDb
+};
 export default loader;
